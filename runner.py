04da9197d5b7131179bc17ea916cc3279c7ad758
nachocano <nachoacano@gmail.com>
2017-03-05 01:38:24 -0800

    adding tests

--- runner.py
+++ runner.py
@@ -4,7 +4,8 @@ import os
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('-e', '--executable', required=True, help='executable file')
 parser.add_argument('-d', '--nr_descriptors', required=True, type=int, help='number of descriptors')
-parser.add_argument('-t', '--max_threads', required=True, type=int, help='max number of threads')
+parser.add_argument('-m', '--max_threads', required=True, type=int, help='max number of threads')
+parser.add_argument('-t', '--times', required=False, type=int, default=5, help='number of times each test executes')
 args = parser.parse_args()
 
 assert args.nr_descriptors > 0
